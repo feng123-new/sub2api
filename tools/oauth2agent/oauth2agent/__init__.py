086ffa82e5a6644aa0afe1c524776d7b6a049ecf
feng123-new <3494617655@qq.com>
2026-08-12 21:47:22 +0800

    feat(oauth2agent): add package exports

---
+++ tools/oauth2agent/oauth2agent/__init__.py
@@ -0,0 +1,4 @@
+from .core import AgentIdentity, OAuth2AgentError, OAuthMaterial, convert_oauth_to_agent_identity
+
+__all__ = ["AgentIdentity", "OAuth2AgentError", "OAuthMaterial", "convert_oauth_to_agent_identity"]
+__version__ = "0.1.0"
